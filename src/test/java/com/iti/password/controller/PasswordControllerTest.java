920175fa17bf573622428bba3329b12dbd8996c3
Klumpp, Raul <raul.klumpp@wirecard.com>
2020-07-07 11:18:17 -0300

    Test implementation

--- src/test/java/com/iti/password/controller/PasswordControllerTest.java
+++ src/test/java/com/iti/password/controller/PasswordControllerTest.java
@@ -1,23 +1,31 @@
 package com.iti.password.controller;
 
+import com.iti.password.service.PasswordService;
 import com.iti.password.utils.PasswordUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PasswordControllerTest {
 
     private MockMvc mockMvc;
+
+    @Mock
+    private PasswordService service;
 
     @InjectMocks
     private PasswordController controller;
@@ -31,13 +39,17 @@ public class PasswordControllerTest {
     }
 
     @Test
-    public void postController_whenValidPassword_thenReturnTRUE() throws Exception {
+    public void postController_whenValidPassword_thenReturnSuccessValidPassword() throws Exception {
+        given(service.apply(PasswordUtils.VALID_PASSWORD))
+                .willReturn(PasswordUtils.createValidMockPassword());
+
         mockMvc.perform(
                 post(PasswordController.BASE_URL)
-                        .contentType(MediaType.TEXT_HTML)
-                        .content(PasswordUtils.VALID_PASSWORD)
-                        .accept(MediaType.TEXT_HTML))
-                .andExpect(status().isOk());
+                        .contentType(MediaType.TEXT_PLAIN_VALUE)
+                        .content(PasswordUtils.VALID_PASSWORD))
+                .andExpect(status().isAccepted())
+                .andExpect(jsonPath("$.valid").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.valid").value(Boolean.TRUE));
     }
 
 }
--- src/test/java/com/iti/password/utils/PasswordUtils.java
+++ src/test/java/com/iti/password/utils/PasswordUtils.java
@@ -1,7 +1,15 @@
 package com.iti.password.utils;
 
+import com.iti.password.model.Password;
+
 public class PasswordUtils {
 
     public static final String VALID_PASSWORD = "AbTp9!fok";
+
+    public static Password createValidMockPassword() {
+        return new Password.PasswordBuilder()
+                            .setValue(VALID_PASSWORD)
+                            .build();
+    }
 
 }
